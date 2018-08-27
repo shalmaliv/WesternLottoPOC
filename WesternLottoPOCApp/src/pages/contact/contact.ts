ffa2db102561a1d9e4f5faafe7b327810e4f025c
Smitesh Wadher <smiteshwadher@admins-Mac-mini.local>
2018-08-27 17:58:55 +0530

    loader added

--- WesternLottoPOCApp/src/pages/contact/contact.ts
+++ WesternLottoPOCApp/src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController ,LoadingController} from 'ionic-angular';
 import { Observable } from "rxjs/Observable";
 import { ApiProvider } from "../../providers/api/api";
 
@@ -11,8 +11,14 @@ import { ApiProvider } from "../../providers/api/api";
 export class ContactPage {
   films: Observable<any>;
 
-  constructor(public navCtrl: NavController, public apiProvider: ApiProvider) {
+  constructor(public navCtrl: NavController, public apiProvider: ApiProvider,public loadingCtrl: LoadingController) 
+  {
+    const loader = this.loadingCtrl.create({
+      content: "Please Wait"
+    });
+      loader.present();
       this.films = this.apiProvider.getFilms();
+      loader.dismiss();
   }
 
 }
--- WesternLottoPOCApp/src/pages/home/home.ts
+++ WesternLottoPOCApp/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Observable } from "rxjs/Observable";
 import { HttpClient } from "@angular/common/http";
 
@@ -12,12 +12,17 @@ export class HomePage {
 films: Observable<any>;
 public testlist:Array<any>;
 
-  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+  constructor(public navCtrl: NavController, public httpClient: HttpClient, public loadingCtrl: LoadingController) {
     debugger;
     this.testlist=[];
+    const loader = this.loadingCtrl.create({
+      content: "Please Wait"
+    });
     this.films = this.httpClient.get('https://swapi.co/api/films');
+    loader.present();
     this.films.subscribe(data => {
      this.testlist=data.results;
+     loader.dismiss();
       console.log('my data: ', data);
     })
 
