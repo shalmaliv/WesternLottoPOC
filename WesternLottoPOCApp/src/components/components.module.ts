7aa4e405bd43122076b1ba5805ee7cfdbac4c4a7
Smitesh Wadher <smiteshwadher@admins-Mac-mini.local>
2018-08-28 11:06:21 +0530

    changes pulled and comitted of home page

--- WesternLottoPOCApp/src/components/components.module.ts
+++ WesternLottoPOCApp/src/components/components.module.ts
@@ -1,14 +1,5 @@
 import { NgModule } from '@angular/core';
 import { LotteryComponent } from './lottery/lottery';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home';
-@NgModule({
-	declarations: [LotteryComponent,
-    HomeComponent],
-	imports: [],
-	exports: [LotteryComponent,
-    HomeComponent]
-=======
 import { VirtualSportsComponent } from './virtual-sports/virtual-sports';
 @NgModule({
 	declarations: [LotteryComponent,
@@ -16,6 +7,5 @@ import { VirtualSportsComponent } from './virtual-sports/virtual-sports';
 	imports: [],
 	exports: [LotteryComponent,
     VirtualSportsComponent]
->>>>>>> 7c8f913d0f7459c4ff5807a428ae471ba42a54b3
 })
 export class ComponentsModule {}
