df1ed6429454554c687fc2661fd3ea69012c9cff
ganeshp <ganeshp@diipl.com>
2018-09-03 11:17:03 +0530

    virtual sports Home page

--- WesternLottoPOCApp/src/components/virtual-sports/virtual-sports.ts
+++ WesternLottoPOCApp/src/components/virtual-sports/virtual-sports.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController ,LoadingController} from 'ionic-angular';
 import { Observable } from "rxjs/Observable";
 import { ApiProvider } from "../../providers/api/api";
+import { VirtualSportsListing} from '../virtual-sports/virtual-sports-mock'
 
 
 @Component({
@@ -10,15 +11,14 @@ import { ApiProvider } from "../../providers/api/api";
 })
 export class VirtualSportsComponent 
 {
-  films: Observable<any>;
-
-  constructor(public navCtrl: NavController, public apiProvider: ApiProvider,public loadingCtrl: LoadingController) 
-  {
-    const loader = this.loadingCtrl.create({
-      content: "Please Wait",
-      duration:2000    
-    });
-      loader.present();
-      this.films = this.apiProvider.getFilms();
+  virtualgames= VirtualSportsListing;
+   constructor(public navCtrl: NavController, public apiProvider: ApiProvider,public loadingCtrl: LoadingController) 
+   {
+  //   const loader = this.loadingCtrl.create({
+  //     content: "Please Wait",
+  //     duration:2000    
+  //   });
+  //     loader.present();
+  //     this.films = this.apiProvider.getFilms();
   }
 }
