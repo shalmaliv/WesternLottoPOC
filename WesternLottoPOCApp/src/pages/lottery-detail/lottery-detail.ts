d41caa30027b176f0d28210945b14027e6d842f1
ganeshp <ganeshp@diipl.com>
2018-09-05 17:57:22 +0530

    keno detial page UI

--- WesternLottoPOCApp/src/pages/lottery-detail/lottery-detail.ts
+++ WesternLottoPOCApp/src/pages/lottery-detail/lottery-detail.ts
@@ -16,7 +16,9 @@ import { BetSlipPage } from "../../pages/bet-slip/bet-slip";
   templateUrl: 'lottery-detail.html',
 })
 export class LotteryDetailPage {
-  numberlist = ["1","2","3","4","5","6","7","8","9","10","11"];
+  lotto511list = ["1","2","3","4","5","6","7","8","9","10","11"];
+  kenolist = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80"];
+  numberlist : any;
   grid=[];
   noOfColsPerRow = 6;
   lotteryNameFromLottery : any;
@@ -33,6 +35,13 @@ export class LotteryDetailPage {
       console.log('ionViewDidLoad LotteryDetailPage');
       console.log('Name retrieved is ', this.lotteryNameFromLottery);
       
+      if(this.lotteryNameFromLottery == 'Lotto 5/11'){
+        this.numberlist = this.lotto511list;
+      }else if(this.lotteryNameFromLottery == "Keno"){
+        this.numberlist = this.kenolist;
+      }
+
+
       let rowNum = 0; //counter to iterate over the rows in the grid
   
       for (let i = 0; i < this.numberlist.length; i+= this.noOfColsPerRow) { //iterate images
